7c16376308fe025c452363fe5fcc756ec1137cfa
Poklonskiy V <waracolyte@gmail.com>
2012-06-10 12:42:43 +0200

    update example file

--- example.php
+++ example.php
@@ -1,5 +1,5 @@
 <?php
-include 'simplices-xml-into-array.php';
+include 'xml_into_array.php';
 # Simple xml without attributes
 $xml = '<?xml version="1.0" encoding="utf-8"?>
 <data>
@@ -8,7 +8,7 @@ $xml = '<?xml version="1.0" encoding="utf-8"?>
 	<extra3>3333</extra3>
 </data>';
 # Only useful tags
-$array = Simplices_XML_into_Array::xml_2_array($xml);
+$array = xml_into_array::xml_2_array($xml);
 /*
 array(
 	'extra1'  => 1111,
@@ -17,7 +17,7 @@ array(
 )
 */
 # Full parsing, array have root element
-$array = Simplices_XML_into_Array::xml_to_array($xml);
+$array = xml_into_array::xml_to_array($xml);
 /*
 array(
 	'data' => array(
@@ -37,7 +37,7 @@ $xml = '<?xml version="1.0" encoding="utf-8"?>
 	<extra>3333</extra>
 </data>';
 # Quick parsing again
-$array = Simplices_XML_into_Array::xml_2_array($xml);
+$array = xml_into_array::xml_2_array($xml);
 /*
 array(
 	'id' => 1001,
@@ -55,7 +55,7 @@ array(
 )
 */
 # More informative array
-$array = Simplices_XML_into_Array::xml_to_array($xml);
+$array = xml_into_array::xml_to_array($xml);
 /*
 array(
 	'data' => array(
@@ -74,7 +74,7 @@ array(
 )
 */
 # Attributes is priority
-$array = Simplices_XML_into_Array::xml_to_array($xml, true, false);
+$array = xml_into_array::xml_to_array($xml, true, false);
 /*
 array(
 	'data' => array(
@@ -102,7 +102,7 @@ array(
 )
 */
 # Attributes will be omitted
-$array = Simplices_XML_into_Array::xml_to_array($xml, false, true);
+$array = xml_into_array::xml_to_array($xml, false, true);
 /*
 array(
 	'data' => array(
@@ -117,7 +117,7 @@ array(
 )
 */
 # Attributes will be omitted, tags will be fulfill role of attributes
-$array = Simplices_XML_into_Array::xml_to_array($xml, false, false);
+$array = xml_into_array::xml_to_array($xml, false, false);
 /*
 array(
 	'data' => array(
